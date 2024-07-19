88bfa3195c65dfba5935b44b713fd994e43bdd35
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-19 14:02:18 +0200

    Ok

--- widget.py
+++ widget.py
@@ -1,19 +1,10 @@
 from mosaico import widget, config
 
-# Create title
 text = widget.createText()
-text.setText(config["name"])
+text.setText(config["text"])
 text.setHexColor(config["color"])
-text.translate(2,2)
-text.setFontHeight(10)
-
-# Create items
-items = []
-for i in range(0, len(config["items"])):
-    items.append(widget.createText())
-    items[i].setFontHeight(6)
-    items[i].setText(config["items"][i])
-    items[i].translate(2, 6+ 7 * (i + 1))
+text.setFont(config["font"])
+text.moveTo(2,25)
 
 def loop():
     pass
