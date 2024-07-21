df315573ecb432674661ba06b330027350c44933
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-21 19:19:21 +0200

    Center horizontal

--- widget.py
+++ widget.py
@@ -4,7 +4,7 @@ text = widget.createText()
 text.setText(config["text"])
 text.setHexColor(config["color"])
 text.setFont(config["font"])
-text.moveTo(2,25)
 
 def loop():
+    text.centerHorizontally()  
     pass
