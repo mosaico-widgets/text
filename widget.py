dd16c31bcaf1a4d269ffd3de4f9b56ca95f084e0
Murkrow <marcocoppola02.mc@gmail.com>
2024-07-21 19:20:04 +0200

    Vertical

--- widget.py
+++ widget.py
@@ -4,6 +4,7 @@ text = widget.createText()
 text.setText(config["text"])
 text.setHexColor(config["color"])
 text.setFont(config["font"])
+text.moveTo(0,30)
 
 def loop():
     text.centerHorizontally()  
